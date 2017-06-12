6c8be0e01cc85e119fc2299614173aed8ae39740
Matthew Lee <mattlkf@outlook.com>
2017-06-12 12:56:59 -0400

    compare DMA and indirect kernels

--- kernel_cmp/main.asm
+++ kernel_cmp/main.asm
@@ -177,6 +177,20 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;			Prepare space on stack for local variables
 			sub.w	#(2*N_LOCALS), SP
 
+;			Configure DMA
+
+;			Set up the DMA destination registers
+			mov.w	#MPY_OP2, &DMA1DAL
+			mov.w	#0, &DMA1DAH
+;			Size of transfer = height of tile
+			mov.w	#9, &DMA1SZ
+;			Set up the DMA trigger to Hardware Multiply
+			mov.w	#DMA1TSEL_29, &DMACTL0
+;			Set control bits
+			mov.w	#DMADT_4+DMADSTINCR_0+DMASRCINCR_3+DMASWDW, &DMA1CTL
+;			Zero out source address high bits
+            mov.w	#0, &DMA1SAH
+
 ;			loop across a
 			mov.w	#a, R4
 loop_guard_a:
@@ -211,55 +225,17 @@ loop_guard_b:
 ; Loop across k
 ;-------------------------------------------------------------------------------
 
-			mov.w	#K, R6
-kernel:
 
-;-------------------------------------------------------------------------------
-; Using indirect auto increment mode
-;-------------------------------------------------------------------------------
-			mov.w	@R5,	&MPY_OP1
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R7
-
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R8
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R9
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R10
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R11
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R12
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R13
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R14
-			mov.w	@R4+, 	&MPY_OP2
-			add.w	&RESULT,R15
-
-			; shift a-ptr to start of next col
-;			sub.w	#(9*2), R4
-			add.w 	#(M2-9*2), R4	; 9*1 tile
-;			add.w 	#(M2), R4	; 9*1 tile
-
-			; shift b-ptr to next row
-			add.w	#N2, R5
-
-			sub.w	#1, R6
-			jnz kernel
-
-			; reset a and b
-			sub.w	#(M2*K), R4
-			sub.w	#(N2*K), R5
-
+;			jmp kernel_indirect ;<-- 2
+			jmp kernel_dma;<--- 2
+kernel_return:
 
 ;-------------------------------------------------------------------------------
 ; Write tile from registers to memory
 ;-------------------------------------------------------------------------------
 
 			; Restore R6 from stack
-			mov.w	0(SP), R6
+			mov.w	0(SP), R6 ;<-- 2
 
 			MOV.W R7,  (N2*0)(R6)
 			MOV.W R8,  (N2*1)(R6)
@@ -287,6 +263,88 @@ loop_end_a:
 
             ; Get rid of the local variables we declared
 			add.w	#(2*N_LOCALS),SP
+
+;-------------------------------------------------------------------------------
+; Kernel 1: Using indirect auto increment mode
+;-------------------------------------------------------------------------------
+
+kernel_indirect:
+			mov.w	#K, R6
+kernel_indirect_loop_k:
+			mov.w	@R5,	&MPY_OP1 ;<---3
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R7
+
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R8
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R9
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R10
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R11
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R12
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R13
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R14
+			mov.w	@R4+, 	&MPY_OP2
+			add.w	&RESULT,R15
+
+			; shift a-ptr to start of next col
+			add.w 	#(M2-9*2), R4	; 9*1 tile
+			; shift b-ptr to next row
+			add.w	#N2, R5
+
+			sub.w	#1, R6
+			jnz kernel_indirect_loop_k; <--- 3
+
+			; reset a and b
+			sub.w	#(M2*K), R4
+			sub.w	#(N2*K), R5
+
+			jmp kernel_return
+
+;-------------------------------------------------------------------------------
+; Kernel 2: Using DMA
+;-------------------------------------------------------------------------------
+
+kernel_dma:
+			mov.w	R4, R6
+			add.w	#2, R6
+			mov.w	R6, &DMA1SAL		; load a-ptr with offset
+
+			mov.w	#K, R6
+kernel_dma_loop_k:
+			mov.w	@R5, &MPY_OP1	; <-- 3b
+			bis.w	#DMAEN, &DMA1CTL	; enable DMA
+			mov.w	@R4, &MPY_OP2		; trigger DMA
+
+			add.w	&RESULT, R7		; 5 cycles
+			add.w	&RESULT, R8		; 5 cycles
+			add.w	&RESULT, R9
+			add.w	&RESULT, R10
+			add.w	&RESULT, R11
+			add.w	&RESULT, R12
+			add.w	&RESULT, R13
+			add.w	&RESULT, R14
+			add.w	&RESULT, R15
+			bic.w	#DMAEN,	&DMA1CTL 	; disable DMA
+
+			add.w	#M2, R4
+			add.w	#M2, &DMA1SAL
+			add.w	#N2, R5
+
+			sub.w	#1, R6
+			jnz kernel_dma_loop_k ;<-- 3b
+
+			; reset a and b
+			sub.w	#(M2*K), R4
+			sub.w	#(N2*K), R5
+
+			jmp kernel_return
+			nop
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
