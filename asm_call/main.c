6bacc82a1c8bf751fb8865ae0bb4507f6e955437
Matthew Lee <mattlkf@outlook.com>
2017-07-21 14:38:53 -0400

    add examples of calling asm functions

--- asm_call/main.c
+++ asm_call/main.c
@@ -3,6 +3,8 @@
 
 extern uint16_t strLen(const char* s1);
 extern int16_t plus_asm(int16_t a, int16_t b, int16_t c);
+extern int16_t self_modifying_code(int16_t x);
+extern void asm_func(int16_t x);
 
 /*
  * main.c
@@ -10,7 +12,9 @@ extern int16_t plus_asm(int16_t a, int16_t b, int16_t c);
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-    int16_t a = 5;
+    asm_func(3);
+
+    int16_t a = self_modifying_code(6);
     int16_t b = 1;
     int16_t c = 3;
 
--- asm_call/str_util.asm
+++ asm_call/str_util.asm
@@ -1,6 +1,7 @@
 	.global strLen
 	.global plus_asm
-
+	.global self_modifying_code
+	.global asm_func
 strLen:
 	mov.w	#0, R13
 strLen_loop:
@@ -11,9 +12,38 @@ strLen_loop:
 	jmp strLen_loop
 strLen_ret:
 	mov.w 	R13, R12
-	reta
+	ret
 
 plus_asm:
 	add.w	R13, R12
 	add.w	R14, R12
-	reta
+	ret
+
+self_modifying_code:
+	; Read in an argument, return that argument.. via a move
+	mov.w 	#2, &(modify_mov + 2)
+	nop
+	nop
+	nop
+modify_mov:
+	mov.w	#5, R12
+	ret
+
+; test if a stack variable can be directly used as a loop counter
+asm_func:
+	mov.w	#0, R15
+
+	add.w	#2, SP
+	mov.w	R12, 0(SP)
+	tst		0(SP)
+	jz		loop_end
+loop_head:
+	add.w	#1, R15
+	nop
+	sub.w	#1, 0(SP)
+	jnz		loop_head
+
+loop_end:
+	nop
+	sub.w	#2, SP
+	ret
